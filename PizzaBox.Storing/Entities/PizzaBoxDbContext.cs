585c6f73fb776e02a1cd83b2cb3d049d27a3626b
Joe Stalnaker <JoeStalnaker@users.noreply.github.com>
2021-04-29 04:51:18 -0400

    compiles, needs controllers

--- PizzaBox.Storing/Entities/PizzaBoxDbContext.cs
+++ PizzaBox.Storing/Entities/PizzaBoxDbContext.cs
@@ -10,12 +10,17 @@ namespace PizzaBox.Storing.Entities
     public DbSet<Pizza> Pizzas { set; get; }
     public DbSet<Store> Stores { set; get; }
     public DbSet<Menu> Menus { set; get; }
+
+    public PizzaBoxDbContext() { }
+    public PizzaBoxDbContext(DbContextOptions<PizzaBoxDbContext> options) { }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseSqlServer(@"Server=tcp:jsrevature001.database.windows.net,1433;Initial Catalog=PizzaBoxDb;Persist Security Info=False;User ID=joestalnaker;Password=Rm26!0%%1lS3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
       //optionsBuilder.UseSqlServer("Server=tcp:heroesapp.database.windows.net,1433;Initial Catalog=HeroesDb;Persist Security Info=False;User ID=dev;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
       //"Server=tcp:jsrevature001.database.windows.net,1433;Initial Catalog=PizzaBoxDb;Persist Security Info=False;User ID=joestalnaker;Password=Rm26!0%%1lS3;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
     }//OnConfiguring(DbContextOptionsBuilder)
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
       base.OnModelCreating(modelBuilder);
